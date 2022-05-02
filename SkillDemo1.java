c35203b8a4156dacd4dc0c8c0532511f9a36b3d8
KevinZheng2222 <kezheng@ucsd.edu>
2022-05-01 18:49:31 -0700

    Added test case

--- SkillDemo1.java
+++ SkillDemo1.java
@@ -1,3 +1,6 @@
 public class SkillDemo1{
 
+    public static int multiply(int x, int y){
+        return x*y;
+    }
 }
---
+++ SkillDemo1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemo1Test {
+    @Test
+    public void multiply() {
+        assertEquals(2, SkillDemo1.multiply(2, 5));
+    }
+}
